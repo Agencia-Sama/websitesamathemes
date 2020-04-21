8324d36e8eccbd5cba81c0eaf274ab7697ea38b7
unknown <you@example.com>
2020-04-21 14:33:33 -0300

    novos commit

--- assets/js/customObject.js
+++ assets/js/customObject.js
@@ -2,6 +2,13 @@ const SMTHEMESOBJECT = {
     //Portfolio (Temas)
     PortFolio: [
         {
+            name: 'Samá v1 Fashion Brown',
+            image: 'assets/images/themes/samav1-fashion-brown.png',
+            url: 'https://integrando.se/temas/e-commerce/loja-integrada/sama-v1-fashion-brown/',
+            sales: 0,
+            rated: 0
+        },
+        {
             name: 'Samá v1 Footwear Black',
             image: 'assets/images/themes/samav1-footwear-black.png',
             url: '#',
@@ -18,13 +25,6 @@ const SMTHEMESOBJECT = {
         {
             name: 'Samá v1 Bijou Goldlight',
             image: 'assets/images/themes/samav1-bijou-goldlight.png',
-            url: '#',
-            sales: 0,
-            rated: 0
-        },
-        {
-            name: 'Samá v1 Fashion Brown',
-            image: 'assets/images/themes/samav1-fashion-brown.png',
             url: '#',
             sales: 0,
             rated: 0
