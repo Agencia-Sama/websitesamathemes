b32713589c0b3f9506dd417db7547d7bc1ab8670
unknown <you@example.com>
2020-04-22 21:05:43 -0300

    Temas

--- assets/js/customObject.js
+++ assets/js/customObject.js
@@ -4,21 +4,21 @@ const SMTHEMESOBJECT = {
         {
             name: 'Samá v1 Bijou Goldlight',
             image: 'assets/images/themes/samav1-bijou-goldlight.png',
-            url: 'https://integrando.se/temas/e-commerce/loja-integrada/sama-v1-bijou-goldlight/',
+            url: 'https://integrando.se/temas/e-commerce/loja-integrada/sama-v1-bijou-goldlight-1713/',
             sales: 0,
             rated: 0
         },
         {
             name: 'Samá v1 Footwear Black',
             image: 'assets/images/themes/samav1-footwear-black.png',
-            url: 'https://integrando.se/temas/e-commerce/loja-integrada/sama-v1-footwear-black/',
-            sales: 0,
+            url: 'https://integrando.se/temas/e-commerce/loja-integrada/sama-v1-footwear-black-1712/',
+            sales: 1,
             rated: 0
         },
         {
             name: 'Samá v1 Cosmetic Pink',
             image: 'assets/images/themes/samav1-cosmetic-pink.png',
-            url: 'https://integrando.se/temas/e-commerce/loja-integrada/sama-v1-cosmetic-pink/',
+            url: 'https://integrando.se/temas/e-commerce/loja-integrada/sama-v1-cosmetic-pink-1711/',
             sales: 0,
             rated: 0
         },
