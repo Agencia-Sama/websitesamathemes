861f5be3f6a41c23b9b5b7b35e6b3f43700ccde3
unknown <you@example.com>
2020-05-26 18:09:41 -0300

    web wahtsapp

--- assets/js/renderObject.js
+++ assets/js/renderObject.js
@@ -16,7 +16,7 @@ const fn = {
                     }
                 }
             });
-            window.open(`https://api.whatsapp.com/send?phone=558188185372&text=${arrayTxt.join('%0a')}`);
+            window.open(`https://web.whatsapp.com/send?phone=558188185372&text=${arrayTxt.join('%0a')}`);
         });
 
         // adjust item select
